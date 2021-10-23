34933521b453269d9571a49d2bdb178846159591
Zhao Ning <774890103@qq.com>
2021-10-23 14:35:10 +0800

    second

---
+++ demo.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-23 14:15:41
+ * @LastEditTime: 2021-10-23 14:33:29
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \undefinedf:\LearningInNJU\code\Zn\git-demo\demo.cpp
+ */
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello, this is my dll";
+} 
